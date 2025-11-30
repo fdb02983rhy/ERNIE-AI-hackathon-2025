1b0f05146fc8d30f2dd2b0f474a052759cb70879
fdb02983rhy <frog.beepers.0n@icloud.com>
2025-11-30 17:30:39 +0900

    hello

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,17 @@
 import AuthButton from '@/components/AuthButton';
 import CalendarManager from '@/components/CalendarManager';
+import VideoStream from '@/components/VideoStream';
 import { auth } from '@/utils/auth';
 
 export default async function Home() {
 	const session = await auth();
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24 gap-8">
+		<main className="flex min-h-screen flex-col items-center justify-center p-8 gap-8">
 			<h1 className="text-4xl font-bold">Google Auth with NextAuth</h1>
 			<AuthButton />
 			{session && <CalendarManager />}
+			<VideoStream />
 		</main>
 	);
 }
